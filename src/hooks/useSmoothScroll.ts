7e7e27fe5a8e5900765c94b619514cc6aa8509ea
Tejaswith <tejaswithreddy0101@gmail.com>
2026-06-15 12:17:41 +0530

    ip

--- src/hooks/useSmoothScroll.ts
+++ src/hooks/useSmoothScroll.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
